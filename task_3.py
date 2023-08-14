50843a4b2d84ed1edc899c6bdb3af1cd0e0bb3cb
MaksTkachenko <maksym_pro@ukr.net>
2023-08-14 21:32:47 +0300

    added task_3

---
+++ task_3.py
@@ -0,0 +1,12 @@
+# Задача 3. Реверс рядка
+
+
+def reverse_string(string):
+
+    if len(string) == 1:
+        return string
+    else:
+        return string[-1] + reverse_string(string[:-1])
+
+
+print(reverse_string("Hello"))  # olleH
